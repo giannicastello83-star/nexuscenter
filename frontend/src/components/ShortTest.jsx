7f06236da2107de5259eb808457d15ddbb074a70
richard <richard@gmail.com>
2025-11-23 04:44:02 +0900

    init

--- frontend/src/components/ShortTest.jsx
+++ frontend/src/components/ShortTest.jsx
@@ -1,35 +1,31 @@
 import { useEffect } from "react";
 
-export default function ContactForm() {
+export default function FormsAppEmbed() {
   useEffect(() => {
     // Create script element
     const script = document.createElement("script");
     script.src = "https://forms.app/cdn/embed.js";
     script.async = true;
     script.defer = true;
-
     script.onload = () => {
-      if (window.formsapp) {
-        new window.formsapp(
-          "69220a708771255e28e67b68",
-          "fullscreen",
-          {},
-          "dapp-job-application-form"
-        );
-      }
+      // Initialize formsapp when script loads
+      new window.formsapp(
+        "69220a708771255e28e67b68",
+        "fullscreen",
+        {},
+        "https://vc3b3ipj.forms.app"
+      );
     };
 
     document.body.appendChild(script);
 
-    // Cleanup (optional)
     return () => {
+      // Optional cleanup if component unmounts
       document.body.removeChild(script);
     };
   }, []);
 
   return (
-    <div>
-      <div formsappId="69220a708771255e28e67b68"></div>
-    </div>
+    <div formsappId="69220a708771255e28e67b68"></div>
   );
 }
